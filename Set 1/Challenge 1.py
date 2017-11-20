5cfa111e288a85830d42567356684ae184c6309e
Matthew Nguyen <matthewnguyen.667@gmail.com>
2017-11-19 21:55:49 -0800

    Challenge 1 complete

---
+++ Set 1/Challenge 1.py
@@ -0,0 +1,16 @@
+### Convert hex to base64
+# Input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# Output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+import base64
+
+# Function to convert hex to base64
+def hex_to_base64(string):
+    # Convert hex to bytes object
+    output = bytes.fromhex(string)
+    # Convert bytes object to base64 (then convert it to a string)
+    return bytes.decode(base64.b64encode(output))
+
+# Testing
+output = hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+assert(output == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
